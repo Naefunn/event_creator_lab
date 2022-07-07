2cb5b6dc63d46821a272eca144bc4079a4760fa3
Naefunn <nathan.d.foster1@gmail.com>
2022-07-07 12:09:06 +0100

    class set up done

---
+++ models/event_list.py
@@ -0,0 +1,6 @@
+from models.event import *
+
+event1 = Event("09.07.2022", "Bad Comedy 1", 10, "dark basement", "Really bad fringe comedy in a dark basement bar")
+event2 = Event("10.07.2022", "Bad Comedy 2", 7, "dark room", "Another bad comedy, this time in a room")
+
+fringe_events = [event1, event2]
